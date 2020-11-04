3f0dbe2c3194eca8d1ff7d6545f28bd139f574b9
Patrik Komanek <pkomanek@redhat.com>
2020-11-04 12:46:10 +0100

    Modifying the operations worker to use new messaging_client.

--- lib/topological_inventory/satellite/operations/worker.rb
+++ lib/topological_inventory/satellite/operations/worker.rb
@@ -1,5 +1,6 @@
 require "manageiq-messaging"
 require "topological_inventory/satellite/logging"
+require "topological_inventory/satellite/messaging_client"
 require "topological_inventory/satellite/receptor/client"
 require "topological_inventory/satellite/operations/processor"
 require "topological_inventory/satellite/operations/source"
@@ -11,18 +12,12 @@ module TopologicalInventory
       class Worker
         include Logging
 
-        def initialize(messaging_client_opts = {})
-          self.messaging_client_opts = default_messaging_opts.merge(messaging_client_opts)
-        end
-
         def run
           receptor_client = TopologicalInventory::Satellite::Receptor::Client.new(:logger => logger)
           receptor_client.start
 
-          # Open a connection to the messaging service
-          client = ManageIQ::Messaging::Client.open(messaging_client_opts)
-
           logger.info("Topological Inventory Satellite Operations worker started...")
+
           client.subscribe_topic(queue_opts) do |message|
             process_message(message, receptor_client)
           end
@@ -33,7 +28,13 @@ module TopologicalInventory
 
         private
 
-        attr_accessor :messaging_client_opts
+        def client
+          @client ||= TopologicalInventory::Satellite::MessagingClient.default.worker_listener
+        end
+
+        def queue_opts
+          TopologicalInventory::Satellite::MessagingClient.default.worker_listener_queue_opts
+        end
 
         def process_message(message, receptor_client)
           Processor.process!(message, receptor_client)
@@ -43,27 +44,6 @@ module TopologicalInventory
         ensure
           message.ack
           TopologicalInventory::Providers::Common::Operations::HealthCheck.touch_file
-        end
-
-        def queue_name
-          "platform.topological-inventory.operations-satellite"
-        end
-
-        def queue_opts
-          {
-            :auto_ack    => false,
-            :max_bytes   => 50_000,
-            :service     => queue_name,
-            :persist_ref => "topological-inventory-operations-satellite"
-          }
-        end
-
-        def default_messaging_opts
-          {
-            :protocol   => :Kafka,
-            :client_ref => "topological-inventory-operations-satellite",
-            :group_ref  => "topological-inventory-operations-satellite"
-          }
         end
       end
     end
